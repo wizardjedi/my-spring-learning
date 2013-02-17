2a33972fc29f249a6a6baa8ec67a87a9477b847e
Sayapin Alexander <wizardjedi@gmail.com>
2013-02-17 23:36:28 +0400

    + Added simple service tests and service listener tests

--- src/test/java/a1s/learn/guava/ServiceTest.java
+++ src/test/java/a1s/learn/guava/ServiceTest.java
@@ -1,9 +1,13 @@
 package a1s.learn.guava;
 
 import com.google.common.util.concurrent.*;
+import com.google.common.util.concurrent.Service.State;
 import java.util.HashSet;
 import java.util.Set;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import junit.framework.TestCase;
+import org.junit.experimental.categories.Categories;
 
 public class ServiceTest extends TestCase {
 	
@@ -12,6 +16,52 @@ public class ServiceTest extends TestCase {
 	public volatile boolean serviceManagerHealthy = false;
 	public volatile boolean serviceManagerStopped = false;
 	public volatile boolean serviceManagerFailure = false;
+	
+	public volatile boolean serviceStarting = false;
+	public volatile boolean serviceRunning = false;
+	public volatile boolean serviceStopping = false;
+	public volatile boolean serviceTerminated = false;
+	public volatile boolean serviceFailure = false;
+
+	public boolean isServiceStarting() {
+		return serviceStarting;
+	}
+
+	public void setServiceStarting(boolean serviceStarting) {
+		this.serviceStarting = serviceStarting;
+	}
+
+	public boolean isServiceRunning() {
+		return serviceRunning;
+	}
+
+	public void setServiceRunning(boolean serviceRunning) {
+		this.serviceRunning = serviceRunning;
+	}
+
+	public boolean isServiceStopping() {
+		return serviceStopping;
+	}
+
+	public void setServiceStopping(boolean serviceStopping) {
+		this.serviceStopping = serviceStopping;
+	}
+
+	public boolean isServiceTerminated() {
+		return serviceTerminated;
+	}
+
+	public void setServiceTerminated(boolean serviceTerminated) {
+		this.serviceTerminated = serviceTerminated;
+	}
+
+	public boolean isServiceFailure() {
+		return serviceFailure;
+	}
+
+	public void setServiceFailure(boolean serviceFailure) {
+		this.serviceFailure = serviceFailure;
+	}
 	
 	public boolean isServiceManagerHealthy() {
 		return serviceManagerHealthy;
@@ -41,6 +91,90 @@ public class ServiceTest extends TestCase {
 		this.serviceManagerFailure = false;
 		this.serviceManagerHealthy = false;
 		this.serviceManagerStopped = false;
+		
+		this.serviceFailure = false;
+		this.serviceRunning = false;
+		this.serviceStarting = false;
+		this.serviceStopping = false;
+		this.serviceTerminated = false;
+	}
+	
+	public void testService(){
+		MyService s1 = new MyService(this);
+		
+		State state = s1.startAndWait();
+		
+		assertEquals(State.RUNNING, state);
+		
+		State state2 = s1.stopAndWait();
+		
+		assertEquals(State.TERMINATED, state2);
+	}
+	
+	public void testServiceListener(){
+		MyService s1 = new MyService(this);
+		
+		s1.addListener(new ServiceListener(this), MoreExecutors.sameThreadExecutor());
+		
+		assertFalse(serviceFailure);
+		assertFalse(serviceStarting);
+		assertFalse(serviceRunning);
+		assertFalse(serviceStopping);
+		assertFalse(serviceTerminated);
+		
+		s1.start();
+		
+		assertFalse(serviceFailure);
+		assertTrue(serviceStarting);
+		assertFalse(serviceRunning);
+		assertFalse(serviceStopping);
+		assertFalse(serviceTerminated);
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex) {
+			//
+		}
+		
+		assertFalse(serviceFailure);
+		assertTrue(serviceStarting);
+		assertTrue(serviceRunning);
+		assertFalse(serviceStopping);
+		assertFalse(serviceTerminated);
+		
+		s1.stop();
+				
+		assertFalse(serviceFailure);
+		assertTrue(serviceStarting);
+		assertTrue(serviceRunning);
+		assertTrue(serviceStopping);
+		assertFalse(serviceTerminated);
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex) {
+			//
+		}
+		
+		assertFalse(serviceFailure);
+		assertTrue(serviceStarting);
+		assertTrue(serviceRunning);
+		assertTrue(serviceStopping);
+		assertTrue(serviceTerminated);
+	}
+	
+	public void testFailedService(){
+		MyService s1 = new MyService(this, true);
+		
+		s1.start();
+		
+		try {
+			Thread.sleep(3000);
+		} catch (InterruptedException ex) {
+			//
+		}
+		
+		assertEquals(State.FAILED, s1.state());
 	}
 	
 	public void testSimpleSuccessServiceManager() {
@@ -169,6 +303,40 @@ public class ServiceTest extends TestCase {
 		assertEquals(Service.State.FAILED, s1.state()); 
 	}
 	
+	private static class ServiceListener implements Service.Listener {
+		private ServiceTest test;
+		
+		public ServiceListener(ServiceTest test) {
+			this.test = test;
+		}
+		
+		@Override
+		public void starting() {
+			test.setServiceStarting(true);
+		}
+
+		@Override
+		public void running() {
+			test.setServiceRunning(true);
+		}
+
+		@Override
+		public void stopping(State from) {
+			test.setServiceStopping(true);
+		}
+
+		@Override
+		public void terminated(State from) {
+			test.setServiceTerminated(true);
+		}
+
+		@Override
+		public void failed(State from, Throwable failure) {
+			test.setServiceFailure(true);
+		}
+		
+	}
+	
 	private static class ServiceManagerListener implements ServiceManager.Listener{
 		private ServiceTest test;
 		
@@ -217,6 +385,13 @@ public class ServiceTest extends TestCase {
 			if (this.failOnStartup) {
 				throw new Exception("Starting up failed");
 			}
+		}
+
+		@Override
+		protected void shutDown() throws Exception {
+			Thread.sleep(2000);
+			
+			super.shutDown();
 		}
 		
 		@Override
