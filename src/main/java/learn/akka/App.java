a270a3cf2d0b4968b394e4c5fa792e8f66c12208
Sayapin Alexander <wizardjedi@gmail.com>
2014-10-12 18:08:52 +0400

    + Implemented handlers for actor lifecycle

--- src/main/java/learn/akka/App.java
+++ src/main/java/learn/akka/App.java
@@ -3,16 +3,30 @@ package learn.akka;
 import akka.actor.ActorRef;
 import akka.actor.ActorSystem;
 import akka.actor.Props;
+import java.util.concurrent.TimeUnit;
 
 public class App {
 
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
+        System.out.println("Actor system create");
+
         ActorSystem system = ActorSystem.create("system");
-		ActorRef actor = system.actorOf(Props.create(SimpleActor.class));
-		
-		actor.tell("Hello", ActorRef.noSender());
-		
-		system.shutdown();
+
+        System.out.println("Actor system created");
+
+        ActorRef actor = system.actorOf(Props.create(SimpleActor.class));
+
+        System.out.println("Actor created");
+
+        actor.tell("Hello", ActorRef.noSender());
+
+        System.out.println("Wait 2 seconds");
+
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println("Shutting down");
+
+        system.shutdown();
     }
 
 }
--- src/main/java/learn/akka/SimpleActor.java
+++ src/main/java/learn/akka/SimpleActor.java
@@ -1,13 +1,36 @@
 package learn.akka;
 
 import akka.actor.UntypedActor;
+import scala.Option;
 
 public class SimpleActor extends UntypedActor {
+
+    @Override
+    public void preRestart(Throwable reason, Option<Object> message) throws Exception {
+        System.out.println("Restarting " + self());
+
+        super.preRestart(reason, message);
+    }
+
+    @Override
+    public void postStop() throws Exception {
+        System.out.println("Stopping " + self());
+
+        super.postStop();
+    }
+
+    @Override
+    public void preStart() throws Exception {
+        System.out.println("Starting " + self());
+
+        super.preStart();
+    }
+
     @Override
     public void onReceive(Object o) throws Exception {
-        System.out.println("I'm actor "+self()+" from "+self().path());
-		
-		System.out.println("I've got message "+o);
+        System.out.println("I'm actor " + self() + " from " + self().path());
+
+        System.out.println("I've got message " + o);
     }
 
 }
