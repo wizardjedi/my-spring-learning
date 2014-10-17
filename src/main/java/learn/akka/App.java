bf5852c2828413dd348c48cab62a7642878217bd
Sayapin Alexander <wizardjedi@gmail.com>
2014-10-17 22:49:46 +0400

    + implemented code for article

--- src/main/java/learn/akka/App.java
+++ src/main/java/learn/akka/App.java
@@ -19,13 +19,29 @@ public class App {
 
         System.out.println("Actor created");
 
-        actor.tell("Hello", ActorRef.noSender());
+        System.out.println("Wait 2 seconds");
+
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println("Born child");
+
+        actor.tell("Born", ActorRef.noSender());
+
+        System.out.println("Wait 2 seconds");
+
+        TimeUnit.SECONDS.sleep(2);
+
+        System.out.println("Kill child");
+
+        actor.tell("Kill", ActorRef.noSender());
+
+        System.out.println("Wait 2 seconds");
+
+        TimeUnit.SECONDS.sleep(2);
 
         System.out.println("Send poison pill");
 
         actor.tell(PoisonPill.getInstance(), ActorRef.noSender());
-
-        System.out.println("Wait 2 seconds");
 
         TimeUnit.SECONDS.sleep(2);
 
--- src/main/java/learn/akka/SimpleActor.java
+++ src/main/java/learn/akka/SimpleActor.java
@@ -1,9 +1,15 @@
 package learn.akka;
 
+import akka.actor.ActorRef;
+import akka.actor.ActorSystem;
+import akka.actor.PoisonPill;
+import akka.actor.Props;
+import akka.actor.Terminated;
 import akka.actor.UntypedActor;
 import scala.Option;
 
 public class SimpleActor extends UntypedActor {
+    protected ActorRef childActor;
 
     @Override
     public void preRestart(Throwable reason, Option<Object> message) throws Exception {
@@ -28,9 +34,25 @@ public class SimpleActor extends UntypedActor {
 
     @Override
     public void onReceive(Object o) throws Exception {
-        System.out.println("I'm actor " + self() + " from " + self().path());
+        if ("Born".equals(o)) {
+            final ActorSystem system = context().system();
 
-        System.out.println("I've got message " + o);
+            childActor = system.actorOf(Props.create(SimpleActor.class), "child");
+
+            getContext().watch(childActor);
+
+            System.out.println("Child created");
+        } else if ("Kill".equals(o)) {
+            System.out.println("Kill child");
+
+            childActor.tell(PoisonPill.getInstance(), self());
+        } else if (o instanceof Terminated) {
+            Terminated t = (Terminated)o;
+
+            System.out.println("Child "+t.actor()+" was killed. And I've got message");
+        } else {
+            System.out.println("Got message "+o);
+        }
     }
 
 }
