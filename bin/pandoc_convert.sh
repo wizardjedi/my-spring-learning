b17e10a9591dc45ee9fa5a816d9476f09ff10ef9
Sayapin Alexander <wizardjedi@gmail.com>
2015-03-14 09:30:03 +0300

    Added latex template

--- bin/pandoc_convert.sh
+++ bin/pandoc_convert.sh
@@ -3,7 +3,7 @@
 LANG=$1
 SYSTEM=$2
 
-/usr/bin/pandoc -f markdown_github+pipe_tables+yaml_metadata_block -s -t latex -o /source/target/doc_$SYSTEM.$LANG.pdf /source/generated/input.md
+/usr/bin/pandoc -f markdown_github+pipe_tables+yaml_metadata_block --template /source/templates/latex.tex -s -t latex -o /source/target/doc_$SYSTEM.$LANG.pdf /source/generated/input.md
 /usr/bin/pandoc -f markdown_github+pipe_tables+yaml_metadata_block -s -t html -o /source/target/doc_$SYSTEM.$LANG.html /source/generated/input.md
 
 chmod -R 777 /source/target/*
