70d5c766e601627a1af9f6a22c14a955a2872120
Sayapin Alexander <wizardjedi@gmail.com>
2015-09-25 20:55:41 +0300

    + Initial project.

---
+++ src/main/java/learn/App.java
@@ -0,0 +1,13 @@
+package learn;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+
+public class App {
+    public static final Logger logger = LoggerFactory.getLogger(App.class);
+    
+    public static void main(String[] args) {
+        logger.info("Application started");
+    }
+}
