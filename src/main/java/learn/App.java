576d7badabdc2fc15a4e677c2869e894b46867c1
Sayapin Alexander <wizardjedi@gmail.com>
2015-09-25 21:14:30 +0300

    + Added simple Job and tasklet

--- src/main/java/learn/App.java
+++ src/main/java/learn/App.java
@@ -1,7 +1,16 @@
 package learn;
 
+import java.util.logging.Level;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.JobParametersBuilder;
+import org.springframework.batch.core.JobParametersInvalidException;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.repository.JobExecutionAlreadyRunningException;
+import org.springframework.batch.core.repository.JobInstanceAlreadyCompleteException;
+import org.springframework.batch.core.repository.JobRestartException;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
 
 
 public class App {
@@ -9,5 +18,27 @@ public class App {
     
     public static void main(String[] args) {
         logger.info("Application started");
+        
+        AnnotationConfigApplicationContext ctx = new AnnotationConfigApplicationContext(Config.class);
+        
+        JobLauncher jobLauncher = ctx.getBean(JobLauncher.class);
+        
+        Job job = ctx.getBean("simpleJob", Job.class);
+        
+        JobParametersBuilder jobParametersBuilder = new JobParametersBuilder();
+        
+        jobParametersBuilder.addString("param1", "param1 value");
+        
+        try {
+            jobLauncher.run(job, jobParametersBuilder.toJobParameters());
+        } catch (JobExecutionAlreadyRunningException e) {
+            logger.error("Already running task",e);
+        } catch (JobRestartException e) {
+            logger.error("Restart task",e);
+        } catch (JobInstanceAlreadyCompleteException e) {
+            logger.error("Already completed task",e);
+        } catch (JobParametersInvalidException e) {
+            logger.error("Param invalid",e);
+        }
     }
 }
---
+++ src/main/java/learn/Config.java
@@ -0,0 +1,81 @@
+package learn;
+
+import org.slf4j.LoggerFactory;
+import org.springframework.batch.core.Job;
+import org.springframework.batch.core.Step;
+import org.springframework.batch.core.StepContribution;
+import org.springframework.batch.core.configuration.annotation.EnableBatchProcessing;
+import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
+import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.RunIdIncrementer;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.dao.MapExecutionContextDao;
+import org.springframework.batch.core.repository.dao.MapJobExecutionDao;
+import org.springframework.batch.core.repository.dao.MapJobInstanceDao;
+import org.springframework.batch.core.repository.dao.MapStepExecutionDao;
+import org.springframework.batch.core.repository.support.SimpleJobRepository;
+import org.springframework.batch.core.scope.context.ChunkContext;
+import org.springframework.batch.core.step.tasklet.Tasklet;
+import org.springframework.batch.repeat.RepeatStatus;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@EnableBatchProcessing
+@Configuration
+public class Config {
+    @Autowired
+    protected JobBuilderFactory jobBuilderFactory;
+
+    @Autowired
+    protected StepBuilderFactory stepBuilderFactory;
+    
+    @Bean
+    public JobRepository jobRepository() {
+        return 
+            new SimpleJobRepository(
+                new MapJobInstanceDao(), 
+                new MapJobExecutionDao(), 
+                new MapStepExecutionDao(), 
+                new MapExecutionContextDao()
+            );
+    }
+    
+    @Bean
+    public JobLauncher jobLauncher() {
+        SimpleJobLauncher simpleJobLauncher = new SimpleJobLauncher();
+        simpleJobLauncher.setJobRepository(jobRepository());
+        
+        return simpleJobLauncher;
+    }
+    
+    @Bean
+    public Job simpleJob() {
+        return
+            jobBuilderFactory
+                .get("job1")
+                .incrementer(new RunIdIncrementer())
+                .start(echoLineStep())
+                .build();
+    }
+    
+    @Bean
+    public Step echoLineStep() {
+        return
+            stepBuilderFactory
+                .get("step1")
+                .tasklet(
+                    new Tasklet() {
+                        @Override
+                        public RepeatStatus execute(StepContribution contribution, ChunkContext chunkContext) throws Exception {
+                            LoggerFactory.getLogger("echoLineTasklet").info("Hello from tasklet");
+                            
+                            return RepeatStatus.FINISHED;
+                        }
+                    }
+                )
+                .build();
+    }
+}
