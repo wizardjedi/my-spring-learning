f84c626b7cda6e21274d8525fa8f6237cb17adc6
Sayapin Alexander <a.sayapin@a1-systems.com>
2016-03-11 12:40:10 +0300

    + Added simple server with greeting and mysql protocol pcap file.

---
+++ src/main/java/com/lrn/nettymysqlprotocol/DiscardServerHandler.java
@@ -0,0 +1,44 @@
+package com.lrn.nettymysqlprotocol;
+
+import io.netty.buffer.ByteBuf;
+import io.netty.buffer.ByteBufUtil;
+import io.netty.channel.ChannelHandlerContext;
+import io.netty.channel.ChannelInboundHandlerAdapter;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class DiscardServerHandler extends ChannelInboundHandlerAdapter { // (1)
+
+    public static final Logger logger = LoggerFactory.getLogger(DiscardServerHandler.class);
+
+    @Override
+    public void channelRead(ChannelHandlerContext ctx, Object msg) { // (2)
+        logger.debug("channel read");
+
+        ByteBuf byteBuf = (ByteBuf)msg;
+
+        if (logger.isTraceEnabled()) {
+            logger.trace("\n{}", ByteBufUtil.prettyHexDump(byteBuf));
+        }
+
+        ByteBuf buffer = ctx.alloc().buffer();
+
+        ByteBuf writeBuffer = buffer.writeBytes("Hello\n".getBytes());
+
+        ctx.writeAndFlush(buffer);
+
+        // Discard the received data silently.
+        ((ByteBuf) msg).release(); // (3)
+    }
+
+    @Override
+    public void exceptionCaught(ChannelHandlerContext ctx, Throwable cause) { // (4)
+        logger.debug("Exception", cause);
+
+        // Close the connection when an exception is raised.
+        cause.printStackTrace();
+        ctx.close();
+    }
+
+
+}
